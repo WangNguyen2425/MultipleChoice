6c020006fd1d92aea33fcb28278283f3f35d9e6c
ThanhKien00 <kien.nt112002@gmail.com>
2023-05-27 15:47:44 +0700

    commit code 05/27/2023

--- src/main/java/com/demo/app/controller/EmailController.java
+++ src/main/java/com/demo/app/controller/EmailController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.spi.ErrorMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
