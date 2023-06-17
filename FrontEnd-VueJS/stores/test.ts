3570c02f9c32466c90cd944b4f0fd8f4536b1849
Thu <hoaithux5k39@gmail.com>
2023-06-17 23:02:48 +0700

    update-fe-vue

--- FrontEnd-VueJS/stores/test.ts
+++ FrontEnd-VueJS/stores/test.ts
@@ -3,6 +3,7 @@ import { api } from "@/apis";
 
 export const useTestStore = defineStore("test", () => {
   const tests = ref<Test[]>([]);
+  const testDetail = ref({});
 
   const getTests = async () => {
     const res = await api.get("/test/list").catch((err) => {
@@ -10,6 +11,46 @@ export const useTestStore = defineStore("test", () => {
       return null;
     });
     tests.value = res?.data || [];
+  };
+
+  const getTestDetail = async (id: number) => {
+    const res = await api.get(`/test/detail/${id}`).catch((err) => {
+      console.log(err);
+      return null;
+    });
+    testDetail.value = res?.data || [];
+  };
+
+  const createTest = async (
+    subjectCode: string,
+    chapterOrders: Array<[]>,
+    questionQuantity: number,
+    testDay: string,
+    duration: number
+  ) => {
+    const res = await api
+      .post("/test/create/random", {
+        subjectCode,
+        chapterOrders,
+        questionQuantity,
+        testDay,
+        duration,
+      })
+      .catch((err) => {});
+  };
+
+  const createTestCheckbox = async (
+    questionIds: Array<[]>,
+    testDay: string,
+    duration: number
+  ) => {
+    const res = await api
+      .post("/test/create", {
+        questionIds,
+        testDay,
+        duration,
+      })
+      .catch((err) => {});
   };
 
   const deleteById = async (id: number) => {
@@ -24,7 +65,11 @@ export const useTestStore = defineStore("test", () => {
 
   return {
     tests,
+    testDetail,
     getTests,
+    getTestDetail,
     deleteById,
+    createTest,
+    createTestCheckbox,
   };
 });
